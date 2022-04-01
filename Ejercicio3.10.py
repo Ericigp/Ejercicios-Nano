4c07d5de76d211b0f28f3dba5dc7555b34db89c6
Ericigp <61890366+Ericigp@users.noreply.github.com>
2022-04-01 11:01:13 +0200

    Update Ejercicio3.10.py
    Actualizado a la resolución correcta del ejercicio

--- Ejercicio3.10.py
+++ Ejercicio3.10.py
@@ -3,7 +3,6 @@
 Created on Fri Mar 25 10:06:06 2022
 @author: Eric-Portatil
 Clase practica nanoelectronica 1/04/22
-
 Sale lo que tendria que salir pero como si estuviera centrado en el 4/-4 preguntar en clase
 """
 #%%
@@ -52,8 +51,8 @@ def mud(V):
 #%%
 """Comienzo a calcular """
 
-Vmin=-8. #Potencial incicial
-Vmax=8. #Potencial final
+Vmin=-10. #Potencial incicial
+Vmax=10. #Potencial final
 Paso=0.05 #Paso entre cada potencial
 Pasostot=int((Vmax-Vmin)/Paso)+1 #Pasos totales dados entre vmin y vmax
 V=np.linspace(Vmin,Vmax,Pasostot) #genero un vector de pasostot numeros comprendidos entre vmax y vmin de pa
@@ -69,7 +68,8 @@ qcs=1.0
 Ef= -2.5
 while(i<Pasostot):
 
-    N0=2.0 #2 pq al principio esta ocupado por 2 electrones
+    N0H=2.0 #2 pq al principio esta ocupado por 2 electrones
+    N0L=0.0
     
     U=0.0
     Uant=-1.0
@@ -77,36 +77,26 @@ while(i<Pasostot):
     mu_s=mus(V[i])
     mu_d=mud(V[i])
     
-    if (abs(Ef-homo)<abs(Ef-lumo)):
-        while((np.abs(Uant-U))>1e-6):
-            N=2.0*(taud*Fermi(U+homo,mu_s )+(taus*Fermi(U+homo, mu_d)))/taut
-            Uant=U
-            U=qcs*(N-N0)
-            U=Uant+alfa*(U-Uant)
-      
-        I1[i]=2.0*q*(Fermi(U+homo, mu_s)-Fermi(U+homo, mu_d))/taut
-        I1[i]/=1e-6
-        
-        i+=1
-        
-    else:
-        while((np.abs(Uant-U))>1e-6):
-            N=2.0*(taud*Fermi(U+lumo,mu_s )+(taus*Fermi(U+lumo, mu_d)))/taut
-            Uant=U
-            U=qcs*(N-N0)
-            U=Uant+alfa*(U-Uant)
-      
-        I1[i]=2.0*q*(Fermi(U+lumo, mu_s)-Fermi(U+lumo, mu_d))/taut
-        I1[i]/=1e-6
-        
-        i+=1
+    while((np.abs(Uant-U))>1e-6):
+        NH =2.0*(taud*Fermi(U+homo,mu_s )+(taus*Fermi(U+homo, mu_d)))/taut
+        NL =2.0*(taud*Fermi(U+lumo,mu_s )+(taus*Fermi(U+lumo, mu_d)))/taut
+        Uant=U
+        U=qcs*((NH-N0H)+(NL-N0L))
+        U=Uant+alfa*(U-Uant)
+    
+    I1[i]=2.0*q*(Fermi(U+lumo, mu_s)-Fermi(U+lumo, mu_d)+(Fermi(U+homo, mu_s)-Fermi(U+homo, mu_d)))/taut
+    I1[i]/=1e-6    
+    
+    i+=1
+    
     
 i=0
 qcs=1.0
 Ef= -3.5
 while(i<Pasostot):
 
-    N0=2.0 #2 pq al principio esta ocupado por 2 electrones
+    N0H=2.0 #2 pq al principio esta ocupado por 2 electrones
+    N0L=0.0
     
     U=0.0
     Uant=-1.0
@@ -114,36 +104,25 @@ while(i<Pasostot):
     mu_s=mus(V[i])
     mu_d=mud(V[i])
     
-    if (abs(Ef-homo)<abs(Ef-lumo)):
-        while((np.abs(Uant-U))>1e-6):
-            N=2.0*(taud*Fermi(U+homo,mu_s )+(taus*Fermi(U+homo, mu_d)))/taut
-            Uant=U
-            U=qcs*(N-N0)
-            U=Uant+alfa*(U-Uant)
-      
-        I2[i]=2.0*q*(Fermi(U+homo, mu_s)-Fermi(U+homo, mu_d))/taut
-        I2[i]/=1e-6
-        
-        i+=1
-        
-    else:
-        while((np.abs(Uant-U))>1e-6):
-            N=2.0*(taud*Fermi(U+lumo,mu_s )+(taus*Fermi(U+lumo, mu_d)))/taut
-            Uant=U
-            U=qcs*(N-N0)
-            U=Uant+alfa*(U-Uant)
-      
-        I2[i]=2.0*q*(Fermi(U+lumo, mu_s)-Fermi(U+lumo, mu_d))/taut
-        I2[i]/=1e-6
-        
-        i+=1
+    while((np.abs(Uant-U))>1e-6):
+        NH =2.0*(taud*Fermi(U+homo,mu_s )+(taus*Fermi(U+homo, mu_d)))/taut
+        NL =2.0*(taud*Fermi(U+lumo,mu_s )+(taus*Fermi(U+lumo, mu_d)))/taut
+        Uant=U
+        U=qcs*((NH-N0H)+(NL-N0L))
+        U=Uant+alfa*(U-Uant)
+    
+    I2[i]=2.0*q*(Fermi(U+lumo, mu_s)-Fermi(U+lumo, mu_d)+(Fermi(U+homo, mu_s)-Fermi(U+homo, mu_d)))/taut
+    I2[i]/=1e-6    
+    
+    i+=1
         
 i=0
 qcs=1.0
 Ef= -5.0
 while(i<Pasostot):
 
-    N0=2.0 #2 pq al principio esta ocupado por 2 electrones
+    N0H=2.0 #2 pq al principio esta ocupado por 2 electrones
+    N0L=0.0
     
     U=0.0
     Uant=-1.0
@@ -151,29 +130,17 @@ while(i<Pasostot):
     mu_s=mus(V[i])
     mu_d=mud(V[i])
     
-    if (abs(Ef-homo)<abs(Ef-lumo)):
-        while((np.abs(Uant-U))>1e-6):
-            N=2.0*(taud*Fermi(U+homo,mu_s )+(taus*Fermi(U+homo, mu_d)))/taut
-            Uant=U
-            U=qcs*(N-N0)
-            U=Uant+alfa*(U-Uant)
-      
-        I3[i]=2.0*q*(Fermi(U+homo, mu_s)-Fermi(U+homo, mu_d))/taut
-        I3[i]/=1e-6
-        
-        i+=1
-        
-    else:
-        while((np.abs(Uant-U))>1e-6):
-            N=2.0*(taud*Fermi(U+lumo,mu_s )+(taus*Fermi(U+lumo, mu_d)))/taut
-            Uant=U
-            U=qcs*(N-N0)
-            U=Uant+alfa*(U-Uant)
-      
-        I3[i]=2.0*q*(Fermi(U+lumo, mu_s)-Fermi(U+lumo, mu_d))/taut
-        I3[i]/=1e-6
-        
-        i+=1
+    while((np.abs(Uant-U))>1e-6):
+        NH =2.0*(taud*Fermi(U+homo,mu_s )+(taus*Fermi(U+homo, mu_d)))/taut
+        NL =2.0*(taud*Fermi(U+lumo,mu_s )+(taus*Fermi(U+lumo, mu_d)))/taut
+        Uant=U
+        U=qcs*((NH-N0H)+(NL-N0L))
+        U=Uant+alfa*(U-Uant)
+    
+    I3[i]=2.0*q*(Fermi(U+lumo, mu_s)-Fermi(U+lumo, mu_d)+(Fermi(U+homo, mu_s)-Fermi(U+homo, mu_d)))/taut
+    I3[i]/=1e-6    
+    
+    i+=1
 
 
 
@@ -186,6 +153,7 @@ plt.plot(V, I3, 'g',label="$E_F=-5.0\,eV$")             # Represento corriente f
 plt.xlabel('V (V)')       # Eje x
 plt.ylabel('I (μA)')        # Eje y
 plt.title('Corriente frente a diferencia de potencial')   # Título
+plt.xlim([-10,10])
 plt.legend()    
 
 """Preparo las variables para reprentar la conductancia"""
@@ -218,4 +186,5 @@ plt.plot(Vds, G3, 'g',label="$E_F=-5.0\,eV$")             # Represento corriente
 plt.xlabel('V (V)')       # Eje x
 plt.ylabel('G (μS)')        # Eje y
 plt.title('Conductancia frente a diferencia de potencial')   # Título
+plt.xlim([-10,10])
 plt.legend()
